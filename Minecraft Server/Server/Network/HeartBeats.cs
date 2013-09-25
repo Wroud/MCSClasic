3dada7911f5d14d305a33327ddc26a34c36c03ca
Wroud <wroud@mail.ru>
2013-09-25 20:25:22 +0400

    Some Fix

--- Minecraft Server/Server/Network/HeartBeats.cs
+++ Minecraft Server/Server/Network/HeartBeats.cs
@@ -12,19 +12,28 @@ namespace Minecraft_Server.Server.Network
     class HeartBeats
     {
         public static WebClient http;
+        public static Thread thread;
         public static void Start()
         {
+            thread = new Thread(Thr);
             http = new WebClient();
+            thread.Start();
             Log.Info("HeartBeat запущен");
+        }
+        private static void Thr()
+        {
             while (true)
             {
-                Time_tick();
+                http.DownloadData("https://minecraft.net/heartbeat.jsp" 
+                    + "?port=" + ServerConf.Config.server_port 
+                    + "&max=" + ServerConf.Config.max_players 
+                    + "&name=" + ServerConf.Config.motd 
+                    + "&public=" + ServerConf.Config.white_list 
+                    + "&version=7" 
+                    + "&salt=" + ServerConf.Config.Salt 
+                    + "&users=0");
                 Thread.Sleep(45000);
             }
-        }
-        private static void Time_tick()
-        {
-            http.DownloadData("https://minecraft.net/heartbeat.jsp" + "?port=" + ServerConf.Config.server_port + "&max=" + ServerConf.Config.max_players + "&name=" + ServerConf.Config.motd + "&public=" + ServerConf.Config.white_list + "&version=7" + "&salt=" + ServerConf.Config.Salt + "&users=0");
         }
     }
 }
