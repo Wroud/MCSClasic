99191ab3ef6ec36141bd6b55b578b0ee4636b905
Russiablackbird <russiablackbird@gmail.com>
2013-09-25 20:04:10 -0700

    Add PermissionsConf.cs

---
+++ Minecraft Server/Framework/Util/PermissionsConf.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text;
+using Minecraft_Server.Server.Client;
+using Minecraft_Server.Server.Main;
+using Minecraft_Server.Server.Network;
+using Minecraft_Server.Framework.Util;
+
+namespace Minecraft_Server.Framework.Util
+{
+    class PermissionsConf
+    {
+    }
+}
