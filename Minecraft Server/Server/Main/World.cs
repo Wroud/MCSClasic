ca8fde8a1ed9fc22dfeddda9f973516858798110
Russiablackbird <russiablackbird@gmail.com>
2013-09-27 09:08:55 -0700

    Add World.cs P.S FILE IS FREE

---
+++ Minecraft Server/Server/Main/World.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+using System.IO;
+
+namespace Minecraft_Server.Server.Main
+{
+    class World
+    {
+
+        public static void ReadMap(byte[] result)
+        {       
+        }
+
+        public static void WriteMap(byte[] result)
+        {
+        }
+
+    }
+}
