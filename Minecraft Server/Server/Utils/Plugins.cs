958ee0959bfc7933bbcff6ceca43ac1cc1f3efb4
Russiablackbird <russiablackbird@gmail.com>
2013-10-02 20:41:35 +0700

    Update Plugins.cs

--- Minecraft Server/Server/Utils/Plugins.cs
+++ Minecraft Server/Server/Utils/Plugins.cs
@@ -8,32 +8,5 @@ namespace Minecraft_Server.Server.Utils
 {
     class Plugins
     {
-        public void CreateBlock(int x, int y, int z, int BlockType)
-        {
-
-        }
-
-        public void DestroyBlock(int x, int y, int z)
-        { 
-        }
-
-        public void Message(String text)
-        {
-        }
-
-        public void KickedPlayer(int ID, String Reasons)
-        {
-        }
-
-        public void BannedPlayer(int ID, String Reasons)
-        {
-        }
-
-        public void UnbannedPlayer(int ID)
-        {
-
-        }
-
-
     }
 }
